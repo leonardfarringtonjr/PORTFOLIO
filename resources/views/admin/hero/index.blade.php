c9d378ccad68c7e741fa58e2ae3f72351873f900
Leonard <aj199269@yahoo.com>
2023-09-30 21:37:21 -0400

    3

--- resources/views/admin/hero/index.blade.php
+++ resources/views/admin/hero/index.blade.php
@@ -1,139 +1,138 @@
 @extends('admin.layouts.layout')
 
 @section('content')
-<div class="main-content">
     <section class="section">
-      <div class="section-header">
-        <div class="section-header-back">
-          <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
-        </div>
-        <h1>Create New Post</h1>
-        <div class="section-header-breadcrumb">
-          <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-          <div class="breadcrumb-item"><a href="#">Posts</a></div>
-          <div class="breadcrumb-item">Create New Post</div>
-        </div>
-      </div>
-
-      <div class="section-body">
-        <h2 class="section-title">Create New Post</h2>
-        <p class="section-lead">
-          On this page you can create a new post and fill in all fields.
-        </p>
-
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h4>Write Your Post</h4>
-              </div>
-              <div class="card-body">
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
-                  <div class="col-sm-12 col-md-7">
-                    <input type="text" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
-                  <div class="col-sm-12 col-md-7">
-                    <select class="form-control selectric">
-                      <option>Tech</option>
-                      <option>News</option>
-                      <option>Political</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Delete Modal</label>
-                  <div class="col-sm-12 col-md-7">
-                    <div class="custom-file">
-                      <input type="file" class="custom-file-input" id="customFile">
-                      <label class="custom-file-label" for="customFile">Choose file</label>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Toggle switch</label>
-                  <div class="col-sm-12 col-md-7">
-                    <label class="custom-switch mt-2">
-                      <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input">
-                      <span class="custom-switch-indicator"></span>
-                      <span class="custom-switch-description">I agree with terms and conditions</span>
-                    </label>
-                  </div>
-                </div>
-
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Date Picker</label>
-                  <div class="col-sm-12 col-md-7">
-                    <input type="text" class="form-control datepicker">
-                  </div>
-                </div>
-
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Select 2</label>
-                  <div class="col-sm-12 col-md-7">
-                    <section class="select2">
-                      <option value="">test1</option>
-                      <option value="">test2</option>
-
-                    </section>
-
-                  </div>
-                </div>
-
-
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">File Input</label>
-                  <div class="col-sm-12 col-md-7">
-                    <button class="btn btn-danger" data-confirm="Realy?|Do you want to continue?" data-confirm-yes="alert('Deleted :)');">Delete</button>
-                  </div>
-                </div>
-
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
-                  <div class="col-sm-12 col-md-7">
-                    <textarea class="summernote"></textarea>
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
-                  <div class="col-sm-12 col-md-7">
-                    <div id="image-preview" class="image-preview">
-                      <label for="image-upload" id="image-label">Choose File</label>
-                      <input type="file" name="image" id="image-upload" />
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
-                  <div class="col-sm-12 col-md-7">
-                    <input type="text" class="form-control inputtags">
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
-                  <div class="col-sm-12 col-md-7">
-                    <select class="form-control selectric">
-                      <option>Publish</option>
-                      <option>Draft</option>
-                      <option>Pending</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="form-group row mb-4">
-                  <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                  <div class="col-sm-12 col-md-7">
-                    <button class="btn btn-primary">Create Post</button>
-                  </div>
-                </div>
-              </div>
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
-          </div>
+            <h1>Create New Post</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Posts</a></div>
+                <div class="breadcrumb-item">Create New Post</div>
+            </div>
         </div>
-      </div>
+
+        <div class="section-body">
+            <h2 class="section-title">Create New Post</h2>
+            <p class="section-lead">
+                On this page you can create a new post and fill in all fields.
+            </p>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Write Your Post</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <select class="form-control selectric">
+                                        <option>Tech</option>
+                                        <option>News</option>
+                                        <option>Political</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Delete Modal</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile">Choose file</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Toggle switch</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <label class="custom-switch mt-2">
+                                        <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input">
+                                        <span class="custom-switch-indicator"></span>
+                                        <span class="custom-switch-description">I agree with terms and conditions</span>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Date Picker</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" class="form-control datepicker">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Select 2</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <section class="select2">
+                                        <option value="">test1</option>
+                                        <option value="">test2</option>
+
+                                    </section>
+
+                                </div>
+                            </div>
+
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">File Input</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <button class="btn btn-danger" data-confirm="Realy?|Do you want to continue?"
+                                        data-confirm-yes="alert('Deleted :)');">Delete</button>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <textarea class="summernote"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <div id="image-preview" class="image-preview">
+                                        <label for="image-upload" id="image-label">Choose File</label>
+                                        <input type="file" name="image" id="image-upload" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tags</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" class="form-control inputtags">
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <select class="form-control selectric">
+                                        <option>Publish</option>
+                                        <option>Draft</option>
+                                        <option>Pending</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                <div class="col-sm-12 col-md-7">
+                                    <button class="btn btn-primary">Create Post</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
-  </div>
 @endsection
