7d12cd6a479864f4b4a9fed1d06ffd99ffe79ddf
Leonard <aj199269@yahoo.com>
2023-10-02 20:11:59 -0400

    4

--- database/migrations/2023_10_02_230402_create_abouts_table.php
+++ database/migrations/2023_10_02_230402_create_abouts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->text('sub_title')->nullable();
+            $table->string('description')->nullable();
+            $table->text('image')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/admin/sections/about/index.blade.php
+++ resources/views/admin/sections/about/index.blade.php
@@ -1,89 +1,80 @@
 @extends('admin.layouts.layout')
 
 @section('content')
-<section class="section">
-    <div class="section-header">
-        <div class="section-header-back">
-            <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+    <section class="section">
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+            </div>
+            <h1>About Section</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="{{ route('admin.about.index') }}">About</a></div>
+            </div>
         </div>
-        <h1>About Section</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="{{route('admin.about.index')}}">About</a></div>
-        </div>
-    </div>
 
-    <div class="section-body">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Update About Section</h4>
-                    </div>
-
-                    <div class="card-body">
-
-                        <form action="" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            {{-- @method('PUT') --}}
-
-                        {{-- TITLE --}}
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
-                            <div class="col-sm-12 col-md-7">
-                                <input type="text" class="form-control" name="title" value="">
-                            </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Update About Section</h4>
                         </div>
 
-                        {{-- SUB-TITLE --}}
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Sub-Title</label>
-                            <div class="col-sm-12 col-md-7">
-                                <textarea id="" class="form-control" style="height: 100px;" name="sub_title"></textarea>
-                            </div>
-                        </div>
+                        <div class="card-body">
 
-                        {{-- PARAGRAPH --}}
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Paragraph</label>
-                            <div class="col-sm-12 col-md-7">
-                                <textarea id="" class="form-control" style="height: 100px;" name="sub_title"></textarea>
-                            </div>
-                        </div>
+                            <form action="" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                {{-- @method('PUT') --}}
 
-                        {{-- HERO IMAGE --}}
-                        {{-- @if ($hero->image) --}}
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Preview Image</label>
-                            <div class="col-sm-12 col-md-7">
-                            <img src="" alt="" class="w-25">
-                            </div>
-                        </div>
-                        {{-- @endif --}}
+                                {{-- TITLE --}}
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <input type="text" class="form-control" name="title" value="">
+                                    </div>
+                                </div>
 
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">About Me Image</label>
-                            <div class="col-sm-12 col-md-7">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="customFile" name="image">
-                                    <label class="custom-file-label" for="customFile">Choose file</label>
-                                  </div>
-                              </div>
-                        </div>
+                                {{-- SUB-TITLE --}}
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Sub-Title</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <textarea id="" class="form-control" style="height: 100px;" name="sub_title"></textarea>
+                                    </div>
+                                </div>
 
-                        {{-- SUBMIT --}}
-                        <div class="form-group row mb-4">
-                            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                            <div class="col-sm-12 col-md-7">
-                              <button class="btn btn-primary">Update</button>
-                            </div>
-                        </div>
+                                {{-- DESCRIPTION --}}
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Description</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <textarea id="" class="summernote" style="height: 100px;" name="description"></textarea>
+                                    </div>
+                                </div>
 
-                        </form>
+                                {{-- ABOUT IMAGE --}}
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div id="image-preview" class="image-preview">
+                                            <label for="image-upload" id="image-label">Choose File</label>
+                                            <input type="file" name="image" id="image-upload" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- SUBMIT --}}
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <button class="btn btn-primary">Update</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
