002db4839ba6b62d1d8a433c6f8232b5dd43a424
Leonard <aj199269@yahoo.com>
2023-10-05 22:24:20 -0400

    1

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -3,11 +3,56 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use Illuminate\Http\Request;
+use File;
 
 class AboutController extends Controller
 {
-    public function index(){
-        return view('admin.sections.about.index');
+    // public function index(Request $request, string $id){
+    public function index(Request $request){
+        $about = About::find(1); // WE SEARCH THE 'ABOUT' MODEL FOR ID '1'
+        return view('admin.sections.about.index', compact('about')); // WE PASS THE VAR 'ABOUT' TO THE VIEW
+    }
+
+    public function update(Request $request, string $id){
+
+        // (1) VALIDATE THE DATA
+        $request->validate([
+            'image' => ['required', 'image'],
+            'title' => ['required', 'max:200'],
+            'sub_title' => ['required', 'max:500'],
+            'description' => ['max: 300']
+        ]);
+
+        // STORES THE ABOUT IMAGE
+        $about_image = About::find(1);
+
+        if($request->hasFile('image')){
+
+            if($about_image && File::exists(public_path($about_image->image))){ // CHECKS IF THERE IS A HERO IMAGE
+                File::delete(public_path($about_image->image)); // DELETES THE OLD FILE
+            }
+
+            $image = $request->file('image');
+            $imageName = rand().$image->getClientOriginalName(); // GRABS THE FILE'S ORIGINAL NAME, THE METHOD RETURNS THE FILENAME AND ITS EXTENSION, IT MAKES SURE EVERY FILE YOU UPLOAD IS UNIQUE
+            $image->move(public_path('/uploads'), $imageName); // STORES THE IMAGE INTO THE UPLOADS FOLDER
+            $imagePath = "/uploads/".$imageName; // SAVES THE IMAGE PATH INTO THE DB
+        }
+
+        // CHECKS IF DATA EXISTS AND THEN CREATES IT OR UPDATES IT
+        About::updateOrCreate(
+            ['id' => $id],
+            [
+                'image' => isset($imagePath) ? $imagePath : $about_image->image,
+                'title' => $request->title,
+                'sub_title' => $request->sub_title,
+                'description' => $request->description,
+            ],
+        );
+
+        toastr()->success('Updated Successfully!', 'Congrats!');
+        return redirect()->back();
+
     }
 }
